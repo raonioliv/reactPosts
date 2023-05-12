724823916201ba47d30a0560705f6bd6ac4a0437
raoni <raoni.rocha@alphasquad.cx>
2023-05-12 17:07:27 -0300

    started migration in home component

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,88 +1,99 @@
 import './styles.css';
 
-import { Component } from 'react';
+import { Component, useState } from 'react';
 
 
 import { loadPosts } from '../../utils/load-posts';
 import { Posts } from '../../components/Posts';
 import { LoadMorePosts } from '../../components/LoadMorePosts'
-import {SearchBar} from '../../components/SearchBar'
-class Home extends Component { 
-  state = { 
+import { SearchBar } from '../../components/SearchBar'
+
+
+const Home = () => {
+  const [posts, setPosts] = useState([])
+  
+  const noMorePosts = page + postsPerPage >= allPosts.length ? true : false
+  return (
+    <section className='container'>
+
+      {!!searchValue && filteredPosts.length > 0 && (
+        <>
+          <h1><b>Buscando por: </b> {searchValue} </h1>
+        </>
+      )}
+
+      <SearchBar handleChange={this.handleChange} inputValue={searchValue} />
+      <Posts posts={filteredPosts} />
+
+      {!!!searchValue && (
+        <>
+          <LoadMorePosts
+            text={'Load more posts'}
+            loadMorePosts={this.loadMorePosts}
+            disabled={noMorePosts}
+          />
+        </>
+      )}
+      {!filteredPosts.length && (
+        <>
+          <h1>Não há resultados para esta busca =( </h1>
+        </>
+      )}
+    </section>
+  )
+
+}
+
+class Home2 extends Component {
+  state = {
     posts: [],
-    allPosts: [], 
-    page: 0, 
+    allPosts: [],
+    page: 0,
     postsPerPage: 20,
     searchValue: ''
   }
 
-  componentDidMount(){ 
+  componentDidMount() {
     this.setPosts()
   }
 
-  setPosts = async () => { 
-    const {page, postsPerPage} = this.state
+  setPosts = async () => {
+    const { page, postsPerPage } = this.state
     const postsAndPhotos = await loadPosts()
     this.setState({
-      posts: postsAndPhotos.slice(page, postsPerPage) , 
+      posts: postsAndPhotos.slice(page, postsPerPage),
       allPosts: postsAndPhotos
     })
   }
 
-  loadMorePosts = () => { 
-    const { 
+  loadMorePosts = () => {
+    const {
       page,
       postsPerPage,
-      allPosts, 
+      allPosts,
       posts,
-     } = this.state
+    } = this.state
 
     const nextPage = page + postsPerPage
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage )
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage)
     posts.push(...nextPosts)
-    this.setState({ 
+    this.setState({
       posts, page: nextPage
-    }) 
+    })
   }
-  handleChange = (event)=>{
-    this.setState({searchValue: event.target.value })
-  } 
+  handleChange = (event) => {
+    this.setState({ searchValue: event.target.value })
+  }
 
-  render(){ 
-    const {posts, postsPerPage, page, allPosts,  searchValue} = this.state
-    const noMorePosts = page + postsPerPage >= allPosts.length ? true : false 
+  render() {
+
 
     const filteredPosts = !!searchValue ? allPosts.filter(post => {
       return post.title.toLowerCase().includes(searchValue.toLowerCase())
-    }) 
-    : posts
+    })
+      : posts
     return (
-      <section className='container'>
 
-        { !!searchValue && filteredPosts.length > 0 &&(
-        <>
-          <h1><b>Buscando por: </b> {searchValue} </h1>
-        </>
-        )} 
-
-        <SearchBar handleChange={this.handleChange} inputValue={searchValue} />
-        <Posts posts={filteredPosts}/>
-
-        {!!!searchValue && (
-          <>
-            <LoadMorePosts 
-            text={'Load more posts'}
-            loadMorePosts={this.loadMorePosts}
-            disabled={noMorePosts}
-            />
-          </>
-        )}
-        { !filteredPosts.length && (
-          <>
-           <h1>Não há resultados para esta busca =( </h1>
-          </>
-        )}
-      </section>
     );
   }
 }
